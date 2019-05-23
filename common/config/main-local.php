57321f3673270d07e954fc73028e7c1bd6b56ac7
Alvaro Escobar <aescobarborrego@gmail.com>
2019-05-23 10:39:14 -0400

    base de datos cambiada a lextool_test para las pruebas

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -9,11 +9,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-<<<<<<< HEAD
-            'dsn' => 'pgsql:host=localhost;port=5432;dbname=lextool',
-=======
             'dsn' => 'pgsql:host=localhost;port=5432;dbname=lextool_test',
->>>>>>> development
             'username' => 'postgres',
             'password' => 'POSTGRES',
             'charset' => 'utf8',
